148deb25837ddd4679b06b18605d884de089e8ff
Finn Julius Stephansen-Smith <finnjs@stud.ntnu.no>
2017-04-03 16:41:50 +0200

    Update style

--- webserver/webapp/urls.py
+++ webserver/webapp/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+import django
 
 from . import views
+import webserver.settings
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^assets/(?P<path>.*)$', django.views.static.serve, {'document_root': webserver.settings.MEDIA_ROOT})
 ]
