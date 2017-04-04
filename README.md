eaf053625d8b8590eeab1a078d1e04da70709146
Finn Julius Stephansen-Smith <finnjs@stud.ntnu.no>
2017-04-04 17:05:26 +0200

    Add code to actually store the mqtt messages in the database. Also style changes to include chart

--- README.md
+++ README.md
@@ -6,3 +6,15 @@ This gon be gud!
 - To store that data in our database
 - To implement logic checking whether that data is within legal levels based on config data from the database
 - To initialize sending of an SMS to interested users if data is not within legal levels
+
+**Installation guide**
+1. `git clone https://github.com/finnss/ttm4115-server.git`
+2. Navigate to the cloned directory
+3. `pip install --upgrade virtualenv` (assuming you have pip)
+4. `virtualenv -p python3 venv`
+5. `source venv/bin/activate`
+6. `pip install django`
+7. `pip install paho`
+8. `python manage.py migrate`
+9. `python manage.py runserver`
+10. Should be running at `localhost:8000`!
--- webserver/webapp/mqtt.py
+++ webserver/webapp/mqtt.py
@@ -1,8 +1,8 @@
 import paho.mqtt.client as mqtt
 from json import loads
-from models import SensorReading
 
-def verify_topic(topic): # almost certainly overkill
+
+def verify_topic(topic):  # almost certainly overkill
     topic = topic.split("/")
     if len(topic) != 4 or topic[0] != "plantlife" or topic[1] != "sensors" or topic[3] != "humidity":
         return -1
@@ -14,22 +14,33 @@ def verify_topic(topic): # almost certainly overkill
     except ValueError:
         return -1
 
+
 def on_connect(client, userdata, rc):
     client.subscribe("plantlife/sensors/+/humidity", qos=2)
 
+
 def on_message(client, userdata, msg):
+    # Need to import these here because the file is loaded in __init__.py and you can't "load" models yet there.
+    from .models import Sensor, SensorReading
+    print('Received a MQTT message!')
+    print(msg)
     data = loads(msg.payload)
     data["sensor_id"] = verify_topic(msg.topic)
     if data["sensor_id"] == -1:
         # Something went wrong
+        print("Something went wrong.")
         pass
     else:
-        data["sensor_id"] #sensor serial number. should match on sensor and server
-        data["humidity"] #float between and including 0. and 1.
-        data["timestamp"] #epoch timestamp
+        sensor = Sensor.objects.get_or_create(serial_number=data["sensor_id"])[0]
+        sensor_reading = SensorReading.objects.create(moisture=data["moisture"], timestamp=data["timestamp"], sensor=sensor)
+        print('Stored!')
+        print(sensor_reading)
+        return sensor_reading
+
 
 client = mqtt.Client(client_id="plantlife-server", clean_session=False)
 client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect("localhost", 1883, 60)
+print("Successfully connected to MQTT broker.")
