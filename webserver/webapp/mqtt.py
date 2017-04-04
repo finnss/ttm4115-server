0280d359bc13e29b741527bbe2e0331022e49b06
RandomGamer342 <rob.342@hotmail.com>
2017-04-04 14:05:16 +0200

    Subscribes to MQTT broker and turns messages into a dict

--- webserver/webapp/mqtt.py
+++ webserver/webapp/mqtt.py
@@ -1,13 +1,34 @@
 import paho.mqtt.client as mqtt
+from json import loads
+from models import SensorReading
+
+def verify_topic(topic): # almost certainly overkill
+    topic = topic.split("/")
+    if len(topic) != 4 or topic[0] != "plantlife" or topic[1] != "sensors" or topic[3] != "humidity":
+        return -1
+    try:
+        res = int(topic[2])
+        if res < 0:
+            return -1
+        return res
+    except ValueError:
+        return -1
 
 def on_connect(client, userdata, rc):
-    client.subscribe("$SYS/#")
+    client.subscribe("plantlife/sensors/+/humidity", qos=2)
 
 def on_message(client, userdata, msg):
-    # Do something
-    pass
+    data = loads(msg.payload)
+    data["sensor_id"] = verify_topic(msg.topic)
+    if data["sensor_id"] == -1:
+        # Something went wrong
+        pass
+    else:
+        data["sensor_id"] #sensor serial number. should match on sensor and server
+        data["humidity"] #float between and including 0. and 1.
+        data["timestamp"] #epoch timestamp
 
-client = mqtt.Client()
+client = mqtt.Client(client_id="plantlife-server", clean_session=False)
 client.on_connect = on_connect
 client.on_message = on_message
 
